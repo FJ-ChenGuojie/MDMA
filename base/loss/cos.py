d6fd2c62adcc8bf69a390ae041646afe3277a9b5
FJ-ChenGuojie <52023581+FJ-ChenGuojie@users.noreply.github.com>
2024-03-15 18:34:32 +0800

    Delete base directory

--- base/loss/cos.py
+++
@@ -1,7 +0,0 @@
-import torch.nn as nn
-
-def cosine(source, target):
-    source, target = source.mean(), target.mean()
-    cos = nn.CosineSimilarity(dim=0)
-    loss = cos(source, target)
-    return loss.mean()
